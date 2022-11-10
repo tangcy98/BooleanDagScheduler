64af7dbc2dcde70955c3a4e9a2817e31a09f500f
Chenyu Tang <tangcy98@163.com>
2022-11-10 22:18:03 +0800

    [2.1] Main entry implementing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include "../src/memory.h"
-#include "../src/booleandag.h"
-#include "../src/scheduler.h"
-#include "../src/importdag.h"
+#include "./src/memory.h"
+#include "./src/booleandag.h"
+#include "./src/scheduler.h"
+#include "./src/importdag.h"
+#include "./ILP/ILP.h"
 #include <cstdio>
 
 using namespace std;
@@ -16,10 +17,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     const char *inputfile = argv[1];
-    int size = atoi(argv[2]);
-    int Bsize = (size + BLOCKCOL - 1) / BLOCKCOL;
+    uint size = atoi(argv[2]);
+    uint Bsize = (size + BLOCKCOL - 1) / BLOCKCOL;
 
     BooleanDag *G = v2booleandag(inputfile);
+
+    int searchbound = LOG2(Bsize < MESHSIZE ? Bsize : MESHSIZE);
+    if ((1<<searchbound) < Bsize) ++searchbound;
 
     
 
