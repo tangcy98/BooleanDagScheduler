bc32b99edd3069713f33dfa2489748a8c44f1179
Chenyu Tang <tangcy98@163.com>
2023-02-10 17:13:30 +0800

    [3.0] Support router(manually simplified) now

--- codegen.sh
+++ codegen.sh
@@ -1,4 +1,4 @@
-benchmarks=(adder cavlc dec div int2float log2 max multiplier priority sin sqrt)
+benchmarks=(adder cavlc dec div int2float log2 max multiplier priority router sin sqrt)
 g++ main.cpp src/*.cpp ILP/ILP.cpp -lOsiClp -lCbc -lClp -o main
 for b in ${benchmarks[@]}
 do
--- simulation/simulate.sh
+++ simulation/simulate.sh
@@ -3,8 +3,9 @@ benchmark=(adder cavlc dec div int2float log2 max multiplier priority router sin
 baknums=(1 2 4 8 16)
 type=(SRAM RRAM)
 
-
-echo "memorytype,memorysize,benchmark,workload,latency(ns),energy(nJ),simdlatency(ns),simdenergy(nJ),oplatency(ns),simdoplatency(ns),opearations(Ops),throughput(GOPS),simdthroughput(GOPS),efficiency(TTOPS/W),simdefficiency(TTOPS/W),temporalutil,simdtemporalutil,maxspatialutil,avgspatialutil,simdmaxspatialutil,simdavgspatialutil"
+rm $1
+touch $1
+echo "memorytype,memorysize,benchmark,workload,latency(ns),energy(nJ),simdlatency(ns),simdenergy(nJ),oplatency(ns),simdoplatency(ns),opearations(Ops),throughput(GOPS),simdthroughput(GOPS),efficiency(TTOPS/W),simdefficiency(TTOPS/W),temporalutil,simdtemporalutil,maxspatialutil,avgspatialutil,simdmaxspatialutil,simdavgspatialutil" >> $1
 for m in ${type[@]}
     do
     for t in ${baknums[@]}
@@ -12,7 +13,8 @@ for m in ${type[@]}
         for j in ${benchmark[@]}
         do
             g++ simulate.cpp ../src/*.cpp ../ILP/ILP.cpp -DBANKNUM=$t -D$m -lOsiClp -lCbc -lClp -o simulate
-            ./simulate ../benchmark/$j.v ${#workload[@]} ${workload[@]}
+            ./simulate ../benchmark/$j.v ${#workload[@]} ${workload[@]} >> $1
+            echo "$m $t $j finish"
         done
     done
 done
