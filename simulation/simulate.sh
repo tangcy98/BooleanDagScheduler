9a60b98d34e50ddeab1ee47b1f9b8c6cd60aa9a2
Chenyu Tang <tangcy98@163.com>
2023-03-07 16:59:15 +0800

    [3.1] Setup for code refactoring

--- simulation/simulate.sh
+++ simulation/simulate.sh
@@ -3,7 +3,7 @@ benchmark=(adder cavlc dec div int2float log2 max multiplier priority router sin
 baknums=(1 2 4 8 16)
 type=(SRAM RRAM)
 
-rm $1
+rm -f $1
 touch $1
 echo "memorytype,memorysize,benchmark,workload,latency(ns),energy(nJ),simdlatency(ns),simdenergy(nJ),oplatency(ns),simdoplatency(ns),opearations(Ops),throughput(GOPS),simdthroughput(GOPS),efficiency(TOPS/W),simdefficiency(TOPS/W),temporalutil,simdtemporalutil,maxspatialutil,avgspatialutil,simdmaxspatialutil,simdavgspatialutil" >> $1
 for m in ${type[@]}
